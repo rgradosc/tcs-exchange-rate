0917c1de45f16aa030e34f127912cc0aa04ff909
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-24 20:40:31 -0500

    Add common and transverse logging layer to project.

---
+++ TCSExchangeRateAPI/src/Transversal/TCSExchangeRateAPI.Transversal.Common/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace TCSExchangeRateAPI.Transversal.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+}

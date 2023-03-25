c6cd0804256ff5ec45afb95edb804a61b40b56c0
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-24 22:28:32 -0500

    Add layer to configure data mapping

---
+++ TCSExchangeRateAPI/src/Transversal/TCSExchangeRateAPI.Transversal.Mapper/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TCSExchangeRateAPI.Transversal.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            
+        }
+    }
+}

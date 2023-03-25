1e43ae0d725a63810458191eb911e679568584b1
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-24 21:09:04 -0500

    Add data connection layer

---
+++ TCSExchangeRateAPI/src/Infrastructure/TCSExchangeRateAPI.Infrastructure.Data/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using TCSExchangeRateAPI.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace TCSExchangeRateAPI.Infrastructure.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        private const string CONNECTION_STRING_NAME = "DefaultConnection";
+        private readonly IConfiguration configuration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                var sqliteConnection = new SqliteConnection();
+                if (sqliteConnection == null) { return null; }
+
+                sqliteConnection.ConnectionString = configuration.GetConnectionString(CONNECTION_STRING_NAME);
+                sqliteConnection.Open();
+                return sqliteConnection;
+            }
+        }
+    }
+}

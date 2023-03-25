5bbcb1024b300e7cffc6c1ff4b6a115c29836af5
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-24 20:19:58 -0500

    Add the Startup class

---
+++ TCSExchangeRateAPI/src/Services/TCSExchangeRateAPI.Services.WebAPI/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace TCSExchangeRateAPI.Services.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

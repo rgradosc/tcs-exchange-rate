a529fd1b1270266a9ee38315c2103d3ed7c6d764
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-25 16:24:52 -0500

    Exposes resources for getting and updating symbols

---
+++ TCSExchangeRateAPI/src/Services/TCSExchangeRateAPI.Services.WebAPI/Controllers/SymbolsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCSExchangeRateAPI.Application.DTO;
+using TCSExchangeRateAPI.Application.Interfaces;
+
+namespace TCSExchangeRateAPI.Services.WebAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SymbolsController : Controller
+    {
+        private readonly ISymbolApplication _symbolApplication;
+
+        public SymbolsController(ISymbolApplication symbolApplication)
+        {
+            _symbolApplication = symbolApplication;
+        }
+
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _symbolApplication.GetAllAsync();
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> UpdateAsync([FromBody] SymbolDTO symbolDTO)
+        {
+            var response = await _symbolApplication.UpdateAsync(symbolDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+    }
+}

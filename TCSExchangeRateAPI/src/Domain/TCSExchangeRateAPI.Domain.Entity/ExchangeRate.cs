cc2e182713fdcb28cf34c13bee6700b313a98db6
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-25 10:03:47 -0500

    Add domain entities for symbols and exchange rates

---
+++ TCSExchangeRateAPI/src/Domain/TCSExchangeRateAPI.Domain.Entity/ExchangeRate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TCSExchangeRateAPI.Domain.Entity
+{
+    public class ExchangeRate
+    {
+        public int Id { get; set; }
+
+        public string BaseCurrency { get; set; }
+
+        public string TargetCurrency { get; set; }
+
+        public decimal BaseRate { get; set; }
+
+        public DateTime DateLastRate { get; set; }
+
+        public bool Active { get; set; }
+
+        public ExchangeRate()
+        {
+            DateLastRate = DateTime.Now;
+        }
+
+        public ExchangeRate(string baseCurrency, string targetCurrency, decimal baseRate)
+        {
+            BaseCurrency = baseCurrency;
+            TargetCurrency = targetCurrency;
+            BaseRate = baseRate;
+            DateLastRate = DateTime.Now;
+            Active = true;
+        }
+    }
+}
---
+++ TCSExchangeRateAPI/src/Domain/TCSExchangeRateAPI.Domain.Entity/Symbol.cs
@@ -0,0 +1,25 @@
+﻿namespace TCSExchangeRateAPI.Domain.Entity
+{
+    public class Symbol
+    {
+        public int Id { get; set; }
+
+        public string SymbolCode { get; set; }
+
+        public string SymbolName { get; set; }
+
+        public bool Active { get; set; }
+
+        public Symbol()
+        {
+            
+        }
+
+        public Symbol(string symbolCode, string symbolName)
+        {
+            SymbolCode = symbolCode;
+            SymbolName = symbolName;
+            Active = true;
+        }
+    }
+}

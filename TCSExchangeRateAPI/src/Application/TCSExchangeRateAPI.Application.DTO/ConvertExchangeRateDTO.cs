0ec6f681cb600df8935b1dee170991cbea854d09
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-25 23:26:08 -0500

    Add DTOs for mapping exchange rates

---
+++ TCSExchangeRateAPI/src/Application/TCSExchangeRateAPI.Application.DTO/ConvertExchangeRateDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace TCSExchangeRateAPI.Application.DTO
+{
+    public class ConvertExchangeRateDTO
+    {
+        public string Base { get; set; }
+
+        public string Target { get; set; }
+
+        public decimal ExchangeRate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal AmountConverted { get; set; }
+
+    }
+}

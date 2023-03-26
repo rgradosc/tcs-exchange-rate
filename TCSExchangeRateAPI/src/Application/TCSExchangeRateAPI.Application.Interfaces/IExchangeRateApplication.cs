5aff1d2d60db6dc8ac585a17d149f6c26f5674d9
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-25 23:27:53 -0500

    Add contract for exchange rate to application

---
+++ TCSExchangeRateAPI/src/Application/TCSExchangeRateAPI.Application.Interfaces/IExchangeRateApplication.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TCSExchangeRateAPI.Application.DTO;
+using TCSExchangeRateAPI.Transversal.Common;
+
+namespace TCSExchangeRateAPI.Application.Interfaces
+{
+    public interface IExchangeRateApplication
+    {
+        Response<ConvertExchangeRateDTO> ConvertExchangeRate(CalculateExchangeRateDTO calculateDTO);
+
+        Response<ListExchangeRateDTO> Get(string baseCurrency, string targetCurrency);
+
+        Response<IEnumerable<ListExchangeRateDTO>> GetAll();
+
+        Response<bool> Update(UpdateExchangeRateDTO updateDTO);
+
+        Task<Response<ConvertExchangeRateDTO>> ConvertExchangeRateAsync(CalculateExchangeRateDTO calculateDTO);
+
+        Task<Response<ListExchangeRateDTO>> GetAsync(string baseCurrency, string targetCurrency);
+
+        Task<Response<IEnumerable<ListExchangeRateDTO>>> GetAllAsync();
+
+        Task<Response<bool>> UpdateAsync(UpdateExchangeRateDTO updateDTO);
+    }
+}

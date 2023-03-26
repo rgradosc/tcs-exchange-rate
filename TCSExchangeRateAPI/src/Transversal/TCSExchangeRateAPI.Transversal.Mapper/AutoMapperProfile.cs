be04c22c53db54e7ee3eb702d3f6c45f60f77864
Raúl Grados Carreño <gnr2092@gmail.com>
2023-03-25 23:29:25 -0500

    Configure mappers for change types

--- TCSExchangeRateAPI/src/Transversal/TCSExchangeRateAPI.Transversal.Mapper/AutoMapperProfile.cs
+++ TCSExchangeRateAPI/src/Transversal/TCSExchangeRateAPI.Transversal.Mapper/AutoMapperProfile.cs
@@ -12,6 +12,18 @@ namespace TCSExchangeRateAPI.Transversal.Mapper
                 .ForMember(x => x.Code, opt => opt.MapFrom(s => s.SymbolCode))
                 .ForMember(x => x.Name, opt => opt.MapFrom(s => s.SymbolName))
                 .ReverseMap();
+
+            CreateMap<ListExchangeRateDTO, ExchangeRate>()
+                .ForMember(x => x.TargetCurrency, opt => opt.MapFrom(s => s.Target))
+                .ForMember(x => x.BaseCurrency, opt => opt.MapFrom(s => s.Base))
+                .ForMember(x => x.BaseRate, opt => opt.MapFrom(s => s.Rate))
+                .ReverseMap();
+
+            CreateMap<UpdateExchangeRateDTO, ExchangeRate>()
+                .ForMember(x => x.BaseRate, opt => opt.MapFrom(s => s.Rate))
+                .ForMember(x => x.BaseCurrency, opt => opt.MapFrom(s => s.Base))
+                .ForMember(x => x.TargetCurrency, opt => opt.MapFrom(s => s.Target))
+                .ReverseMap();
         }
     }
 }
